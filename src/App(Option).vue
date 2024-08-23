4560bb6fa0a98ea1aef74e4d82136ff2f1938dbf
yawtalkstechs <yawtalkstechs@gmail.com>
2024-08-23 19:33:48 +0000

    vue job

---
+++ src/App(Option).vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+    data() {
+      return {
+        name: "JoHn Noah",
+        status: "pending",
+        fruits: ["Apple", "Banana", "Orange"],
+        urls: "https://www.google.com"
+      };
+    },
+    methods: {
+      toggleStatus() {
+        if (this.status === 'active'){
+          this.status = 'pending';
+        }
+        else if (this.status === 'pending'){
+          this.status = 'inactive'
+        }
+        else {
+          this.status = 'active'
+        }
+      }
+    }
+}
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status==='active'">User is active</p>
+  <p v-else-if="status==='pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <h3>Fruits: </h3>
+  <ul>
+    <li v-for="fruit in fruits" :key="fruit">{{ fruit }}</li>
+  </ul>
+  <a :href="link">Google</a>
+
+  <button @click="toggleStatus">Change status</button>
+</template>
---
+++ src/components/JobListings.vue
@@ -0,0 +1,60 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+// import JobData from '@/jobs2.json';
+import { reactive, onMounted } from 'vue';
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
+import JobListing from './JobListing.vue';
+import axios from 'axios';
+
+defineProps({
+    limit: Number,
+    showButton: {
+        type: Boolean,
+        default: false,
+    },
+})
+
+const state = reactive({
+    jobs: [],
+    isLoading: true
+});
+
+
+onMounted(async () => {
+    try {
+        const response = await axios.get('/api/jobs');
+        state.jobs = response.data;
+    } catch (error) {
+        console.error('Could not fetch data from server', error); 
+    }
+    finally {
+        state.isLoading = false
+    }
+});
+</script>
+
+<template>
+    <section class="bg-blue-50 px-4 py-10">
+        <div class="container-xl lg:container m-auto">
+            <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+                Browse Jobs
+            </h2>
+            
+            <!-- Show Spinner -->
+             <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+                <PulseLoader />
+             </div>
+
+             <!-- Show Job Listing -->
+
+            <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id" :job="job" />
+
+            </div>
+        </div>
+    </section>
+    <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+        <RouterLink to="/jobs" class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">View
+            All Jobs</RouterLink>
+    </section>
+</template>
